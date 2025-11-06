1507321cacd84d15c7c98e151a82613548376518
Jalil Artykov <artykovjalil@gmail.com>
2025-11-06 18:11:41 +0600

    api for urls

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const BINAR_API_URL = import.meta.env.VITE_BINAR_API_URL || 'https://health-binar-ikigai-api-production.up.railway.app'
-const STORE_API_URL = import.meta.env.VITE_STORE_API_URL || 'https://catalog-api-production-a903.up.railway.app'
+const BINAR_API_URL = import.meta.env.VITE_BINAR_API_URL || 'https://health-binar-ikigai-api-production.up.railway.app/api'
+const STORE_API_URL = import.meta.env.VITE_STORE_API_URL || 'https://catalog-api-production-a903.up.railway.app/api'
 
 // binar_api for all pages except Products.vue
 export const binar_api = axios.create({
